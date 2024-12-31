f46f854ee42c2bc6ca1025cda175d476bbc20abf
Matheus <43830739+Matheuscs787@users.noreply.github.com>
2024-12-31 16:55:11 -0300

    Criação do repositório

---
+++ src/main/java/br/com/earnings/MyEarningsApplication.java
@@ -0,0 +1,13 @@
+package br.com.earnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEarningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEarningsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/earnings/MyEarningsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.earnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEarningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
